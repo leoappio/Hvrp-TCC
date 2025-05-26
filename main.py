9771f8cfed15e0cde6d974c1ad00f3b28041fc92
Leonardo Appio <leonardo@okai.com.br>
2025-05-25 22:55:00 -0300

    added another tests

--- main.py
+++ main.py
@@ -42,41 +42,50 @@ def euclidean(i, j, coords):
     return round(math.hypot(x1 - x2, y1 - y2), 2)
 
 
+def configure_hvrp_problem(problem_number: int):
+    vehicles_3_5 = {
+        "A": {"capacity": 20, "fixed_cost": 20, "var_cost": 1},
+        "B": {"capacity": 30, "fixed_cost": 35, "var_cost": 1},
+        "C": {"capacity": 40, "fixed_cost": 50, "var_cost": 1},
+        "D": {"capacity": 70, "fixed_cost": 120, "var_cost": 1},
+        "E": {"capacity": 120, "fixed_cost": 225, "var_cost": 1},
+    }
+
+    vehicles_4_6 = {
+        "A": {"capacity": 60, "fixed_cost": 1000, "var_cost": 1},
+        "B": {"capacity": 80, "fixed_cost": 1500, "var_cost": 1},
+        "C": {"capacity": 150, "fixed_cost": 3000, "var_cost": 1},
+    }
+
+    depot_coord_default = (30, 40)
+    depot_coord_20_20 = (20, 20)
+
+    vehicles = vehicles_3_5 if problem_number in [3, 5] else vehicles_4_6
+    depot_coord = depot_coord_default if problem_number in [3, 4] else depot_coord_20_20
+
+    return vehicles, depot_coord
+
+
+problem_number = 4
 
 coords, demands = read_hvrp_instance("tests/E-n20-k5.vrp")
+
+vehicle_types, new_depot_coord = configure_hvrp_problem(problem_number)
+coords[1] = new_depot_coord
+
 nodes = list(coords.keys())
 depot = 1
 clients = [i for i in nodes if i != depot]
 edges = [(i, j) for i in nodes for j in nodes if i != j]
 
-vehicle_types = {
-    "A": {"capacity": 20, "fixed_cost": 20, "var_cost": 1},
-    "B": {"capacity": 30, "fixed_cost": 35, "var_cost": 1},
-    "C": {"capacity": 40, "fixed_cost": 50, "var_cost": 1},
-    "D": {"capacity": 70, "fixed_cost": 120, "var_cost": 1},
-    "E": {"capacity": 120, "fixed_cost": 225, "var_cost": 1},
-}
-
 model = pulp.LpProblem("HVRP_Model", pulp.LpMinimize)
 
-
-# y[i,j,k] = 1 se arco (i,j) é usado pelo veículo tipo k
 y = pulp.LpVariable.dicts("y", [(i, j, k) for (i, j) in edges for k in vehicle_types], cat="Binary")
-
-
-# a[i,k] = 1 se o cliente i é o último da rota com veículo k
 a = pulp.LpVariable.dicts("a", [(i, k) for i in clients for k in vehicle_types], cat="Binary")
-
-
-# b[i,k] = 1 se o cliente i é visitado por veículo k e não é o último
 b = pulp.LpVariable.dicts("b", [(i, k) for i in clients for k in vehicle_types], cat="Binary")
-
-
-# v[i,k] = carga acumulada no veículo k ao chegar em i
 v = pulp.LpVariable.dicts("v", [(i, k) for i in nodes for k in vehicle_types], lowBound=0, cat="Continuous")
 
-
-# funçao objetivo
+# Função objetivo
 model += (
     pulp.lpSum(
         a[i, k] * (vehicle_types[k]["fixed_cost"] + vehicle_types[k]["var_cost"] * euclidean(i, depot, coords))
@@ -88,26 +97,19 @@ model += (
     )
 )
 
-
-# cada cliente atendido uma unica vez
+# Restrições
 for i in clients:
     model += pulp.lpSum(a[i, k] + b[i, k] for k in vehicle_types) == 1
 
-
-# conservacao de fluxo
 for i in clients:
     for k in vehicle_types:
         model += pulp.lpSum(y[j, i, k] for j in nodes if j != i) == a[i, k] + b[i, k]
         model += pulp.lpSum(y[i, j, k] for j in nodes if j != i) == b[i, k]
 
-
-# veículo sai e retorna do depósito no máximo uma vez
 for k in vehicle_types:
     model += pulp.lpSum(y[depot, j, k] for j in clients) <= 1
     model += pulp.lpSum(y[i, depot, k] for i in clients) <= 1
 
-
-# restrições de capacidade com carga acumulada
 for k in vehicle_types:
     Q = vehicle_types[k]["capacity"]
     for i in clients:
